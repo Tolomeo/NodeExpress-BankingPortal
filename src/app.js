b46ec52a66cb2354aec5d55d0ac548ea4f2c8cef
Diego Frattini <dgfrattini@gmail.com>
2020-09-11 17:53:45 +0100

    Creating a Data Access Library

--- src/app.js
+++ src/app.js
@@ -1,19 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const { accounts, users, writeJSON } = require('./data');
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
-const accountData = fs.readFileSync(path.resolve("src/json", "accounts.json"), {
-  encoding: "utf8",
-});
-const accounts = JSON.parse(accountData);
-const userData = fs.readFileSync(path.resolve("src/json", "users.json"), { encoding: "utf8" });
-const users = JSON.parse(userData);
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Account Summary", accounts });
@@ -49,8 +43,7 @@ app.post("/transfer", (req, res) => {
   accounts[to].prior_balance = accounts[to].balance;
   accounts[to].balance += amount;
 
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, 'utf8');
+  writeJSON();
 
   res.render("transfer", { message: "Transfer Completed" });
 });
@@ -66,8 +59,7 @@ app.post("/payment", (req, res) => {
   accounts.credit.balance -= amount;
   accounts.credit.available += amount;
 
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, 'utf8');
+  writeJSON();
 
   res.render("payment", { message: "Payment Successful", account: accounts.credit });
 });
